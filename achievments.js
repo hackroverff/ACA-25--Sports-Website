6656db60d4e9f4f3a7b9c6b8ace93f27f413210f
hackroverff <professional.kingdommvp@gmail.com>
2021-09-14 18:43:27 +0530

    ACA-26--Sports-Website

--- achievments.js
+++ achievments.js
@@ -22,13 +22,23 @@ function readResults(year){
     var results=firebase.database().ref(year) ;
     results.on('value', (data) => {
         var ranks = data.val();
-        document.getElementById("result1").innerHTML ="<br > 1. " +ranks[1];
-        document.getElementById("result2").innerHTML ="<br > 2. " +ranks[2];
-        document.getElementById("result3").innerHTML ="<br > 3. " +ranks[3];
-        document.getElementById("result4").innerHTML ="<br > 4. " +ranks[4];
-        document.getElementById("result5").innerHTML ="<br > 5. " +ranks[5];
-        document.getElementById("result6").innerHTML ="<br > 6. " +ranks[6];
-        document.getElementById("result7").innerHTML ="<br > 7. " +ranks[7];
-        document.getElementById("result8").innerHTML ="<br > 8. " +ranks[8];
+        var finalranks=ranks.toString().split(',');
+        var text='';
+        for(var i=1; i<ranks.length; i++){
+          // text=text+i+". "+ranks[i]+"<br>"
+          document.getElementById('result'+i).innerHTML = i+". "+finalranks[i]+"<br>";
+          console.log(i)
+          console.log(year)
+          console.log(finalranks[i])
+        }
+        // document.getElementById('result1').innerHTML = text;
+        // document.getElementById("result1").innerHTML ="<br > 1. " +ranks[1];
+        // document.getElementById("result2").innerHTML ="<br > 2. " +ranks[2];
+        // document.getElementById("result3").innerHTML ="<br > 3. " +ranks[3];
+        // document.getElementById("result4").innerHTML ="<br > 4. " +ranks[4];
+        // document.getElementById("result5").innerHTML ="<br > 5. " +ranks[5];
+        // document.getElementById("result6").innerHTML ="<br > 6. " +ranks[6];
+        // document.getElementById("result7").innerHTML ="<br > 7. " +ranks[7];
+        // document.getElementById("result8").innerHTML ="<br > 8. " +ranks[8];
           })
   }
